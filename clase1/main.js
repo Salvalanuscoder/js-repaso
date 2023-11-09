ce43158b6d80e13f717c235d74b4913b7dcf20aa
Salvador Lanús <salva@Salvas-MacBook-Pro.local>
2023-11-09 14:16:59 -0300

    first commit

---
+++ clase1/main.js
@@ -0,0 +1,46 @@
+//comentario de linea
+
+/*
+comentario
+ de 
+ bloque
+*/
+
+//declaracion de una variable
+// let edad = 19;//forma recomendada
+// var nombre = 'salva';//forma ilegal
+// //string es lo que se encuentera dentro del '', ej:'salva'
+// const DIRECCION = 'AV';
+
+// console.log("buenas")
+// console.log("edad");
+// console.log("nombre");
+// console.log("DIRECCION");
+
+// let edad = 0;
+// let nombre = "salva";
+
+// console.log(typeof(edad));
+// console.log(typeof(nombre));
+
+// nombrePersonaBonita //Camel Case
+// nombre_persona_bonita//Snake Case
+// NombrePersonaBonita//Pascal Case
+
+// let numeroUno = 4;
+// let numeroDos = 1;
+
+// let suma =  numeroUno + numeroDos;
+// let resta =  numeroUno - numeroDos;
+// let multiplicacion =  numeroUno * numeroDos;
+// let division =  numeroUno / numeroDos;
+// console.log(suma, resta, multiplicacion, division);
+
+// let saludo = "hola";
+// let nombre = "salva";
+
+// let saludoCompleto = saludo + " " + nombre;
+// console.log(saludoCompleto);
+
+let nombre = prompt("Ingrese el nombre");
+alert(nombre);
